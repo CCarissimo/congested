18fcbf66dbd16d2d9d9dfd58279f89a109ce53a7
ccarissimo <N20kztu!>
2023-01-16 16:51:05 +0100

    deprecated code

--- run_recommender.py
+++ run_recommender.py
@@ -5,15 +5,16 @@
 
 
 def main():
-    from functions import vecSOrun, plot_run, vecSOrun_recommender
+    from functions import vecSOrun, plot_run, vecSOrun_recommender, total_welfare, total_updates, vecSOrun_heuristic_recommender
     import numpy as np
+    # from recommenders import vecSOrun_heuristic_recommender
 
     N_AGENTS = 100
     N_STATES = 3
     N_ACTIONS = 3
     N_ITER = 5000
 
-    EPSILON = 0.01
+    EPSILON = 0.15
     mask = np.zeros(N_AGENTS)
     mask[:] = 1
     GAMMA = 0
@@ -25,9 +26,13 @@ def main():
     SELECT_TYPE = "EPSILON"  ## "EPSILON" or "GNET"
     WELFARE_TYPE = "AVERAGE"  ## "AVERAGE" or "MIN" or "MAX"
 
-    NAME = f"run_{PAYOFF_TYPE}_{SELECT_TYPE}_{WELFARE_TYPE}_N{N_AGENTS}_S{N_STATES}_A{N_ACTIONS}_I{N_ITER}_e{EPSILON}_g{GAMMA}_a{ALPHA}_q{QINIT} "
+    NAME = f"run_{PAYOFF_TYPE}_{SELECT_TYPE}_{WELFARE_TYPE}_N{N_AGENTS}_S{N_STATES}_A{N_ACTIONS}_I{N_ITER}_e{EPSILON}_g{GAMMA}_a{ALPHA}_q{QINIT}"
 
-    M, Q = vecSOrun_recommender(N_AGENTS, N_STATES, N_ACTIONS, N_ITER, EPSILON, mask, GAMMA, ALPHA, QINIT, PAYOFF_TYPE, SELECT_TYPE)
+    random_recommender = False
+    recommender_objective = None
+
+    M, Q = vecSOrun_heuristic_recommender(N_AGENTS, N_STATES, N_ACTIONS, N_ITER, EPSILON, GAMMA, ALPHA, QINIT,
+                                                PAYOFF_TYPE, SELECT_TYPE, random_recommender, recommender_objective)
     T0 = np.mean([M[t]["R"] for t in range(int(0.8 * N_ITER), N_ITER)])
     T1 = np.mean([np.mean(M[t]["R"]) for t in range(int(0.8 * N_ITER), N_ITER)])
 
