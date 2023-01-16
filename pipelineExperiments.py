27e5999f346c8880ea4f7011aeaab9bbe9a0a685
ccarissimo <N20kztu!>
2023-01-16 16:51:31 +0100

    pipeline updated for alignment experiments

--- pipelineExperiments.py
+++ pipelineExperiments.py
@@ -10,9 +10,10 @@ def main():
     import pickle
     import nolds
     import pandas as pd
-    from recommenders import heuristic_recommender, naive_recommender, random_recommender
+    from recommenders import heuristic_recommender, naive_recommender, random_recommender, constant_recommender
     from single_run import single_run
     from routing_networks import braess_augmented_network
+    from run_functions import calculate_alignment
 
     # Base Settings Which Will Not Change
     N_AGENTS = 100
@@ -23,18 +24,19 @@ def main():
     mask = np.zeros(N_AGENTS)
     mask[:] = 1
     GAMMA = 0
-    ALPHA = 0.01
+    ALPHA = 0.1
 
     # Parameters which will be Varied
     EPSILON = "Variable"
-    sizeEpsilon = 1  #11
-    epsilons = np.linspace(0, 1, sizeEpsilon)
+    sizeEpsilon = 1  # 18
+    epsilons = [0]  # [0, 0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1, 0.15, 0.3, 0.45, 0.6, 0.75, 0.9, 1]  # np.linspace(0, 1, sizeEpsilon)
     
     QINIT = "Variable"
     sizeQinit = 1
     qinits = {
-        "uniform": "UNIFORM",
+        # "uniform": "UNIFORM",
         # "nash": np.array([-2, -2, -2]),
+        "aligned": "ALIGNED",
         # "cdu": np.array([-2, -1.5, -1]),
         # "cud": np.array([-1.5, -2, -1]),
         # "ucd": np.array([-1, -2, -1.5]),
@@ -43,16 +45,23 @@ def main():
         # "duc": np.array([-1.5, -1, -2])
     }
 
+    recommenders = {
+        "heuristic": heuristic_recommender,
+        "naive": naive_recommender,
+        "random": random_recommender,
+        "none": constant_recommender,
+    }
+
     NAME = f"sweep_e{sizeEpsilon}_q{sizeQinit}_N{N_AGENTS}_S{N_STATES}_A{N_ACTIONS}_I{N_ITER}_e{EPSILON}_g{GAMMA}_a{ALPHA}_q{QINIT}"
 
     results = []
 
     for i, e in enumerate(tqdm.tqdm(epsilons)):
         for norm, initTable in qinits.items():
-            for recommender in [heuristic_recommender, naive_recommender, random_recommender]:
+            for recommender_type, recommender_function in recommenders.items():
                 for t in range(0, N_REPEATS):
                     M = single_run(braess_augmented_network, N_AGENTS, N_STATES, N_ACTIONS, N_ITER, e, GAMMA,
-                                   ALPHA, initTable, recommender, trusting=False)
+                                   ALPHA, initTable, recommender_function)
                     W = [M[t]["R"].mean() for t in range(0, N_ITER)]
                     L = nolds.lyap_r(W)
                     T = np.mean(W[int(0.8 * N_ITER):N_ITER])
@@ -64,9 +73,11 @@ def main():
                     Qvar = [M[t]["Qvar"] for t in range(0, N_ITER)]
                     Qvar_mean = np.mean(Qvar)
 
-                    # M, Q = vecSOrun_states(N_AGENTS, N_STATES, N_ACTIONS, NEIGHBOURS, 1, 0, mask, GAMMA, ALPHA, Q,
-                    #                        PAYOFF_TYPE, SELECT_TYPE)
-                    # oneShot = np.mean(M[0]["R"])
+                    if recommender_type == "none":
+                        alignment = [None, None, None]
+                    else:
+                        alignment = np.array([M[t]["alignment"] for t in range(int(0.8 * N_ITER), N_ITER)])
+                        alignment = alignment.mean(axis=0)
 
                     row = {
                         "epsilon": e,
@@ -79,7 +90,10 @@ def main():
                         "groups_mean": groups_mean,
                         "groups_var": groups_var,
                         "Qvar_mean": Qvar_mean,
-                        "random_recommender": random_recommender,
+                        "recommender_type": recommender_type,
+                        "alignment_up": alignment[0],
+                        "alignment_down": alignment[1],
+                        "alignment_cross": alignment[2],
                     }
 
                     results.append(row)
