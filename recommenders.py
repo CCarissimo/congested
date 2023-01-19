af438edd2c24e1d5015508389b97177c7965d7ed
ccarissimo <N20kztu!>
2023-01-19 11:51:19 +0100

    added optimized recommender functions

--- recommenders.py
+++ recommenders.py
@@ -82,6 +82,146 @@ def heuristic_recommender(Q, n_agents):
     return S.astype(int)
 
 
+def split_up_down_cross_flexible(arg_max_Q):
+    up = []
+    down = []
+    cross = []
+    flexible = []
+
+    for i, argmax_q_table in enumerate(arg_max_Q):
+        if 0 in argmax_q_table:  # could the agent go up?
+            if 1 in argmax_q_table:  # could the agent go down?
+                flexible.append(i)  # if both, store for later assignment
+            else:
+                up.append(i)  # if only up, assign agent to go up
+        elif 1 in argmax_q_table:
+            down.append(i)  # if only down, assign agent to go down
+        else:
+            cross.append(i)  # add logic for sure crossers
+
+    return up, down, cross, flexible
+
+
+def sort_by_belief_difference(Q, arg_max_Q, agent_indices, action):
+    ind, state = np.where(arg_max_Q[agent_indices] == action)
+
+    values = Q[agent_indices][ind, state, action] - Q[agent_indices][ind, state, 2]
+
+    structured_array = np.array(
+        list(zip(values, ind, state)),
+        dtype=[("belief_diff", float), ("index", int), ("state", int)]
+    )
+    structured_array.sort(order="belief_diff")
+
+    sorted_agent_indices = []
+    sorted_agent_recommendations = []
+
+    for agent in reversed(structured_array):
+        agent_index = agent_indices[agent[1]]
+        recommendation = agent[2]
+        if agent_index in sorted_agent_indices:
+            continue
+        else:
+            sorted_agent_indices.append(agent_index)
+            sorted_agent_recommendations.append(recommendation)
+
+    return sorted_agent_indices, sorted_agent_recommendations
+
+
+def sort_by_belief_improvement(Q, arg_max_Q, agent_indices, action, method, travel_time_estimate, minimize=False):
+    coefficient = -1 if minimize else 1
+
+    ind, state = np.where(arg_max_Q[agent_indices] == action)
+
+    target = travel_time_estimate[action] if method == "estimate" else Q[agent_indices][ind, state, 2]
+
+    values = coefficient * (- target - Q[agent_indices][ind, state, action])
+
+    structured_array = np.array(
+        list(zip(values, ind, state)),
+        dtype=[("belief_diff", float), ("index", int), ("state", int)]
+    )
+    structured_array.sort(order="belief_diff")
+
+    sorted_agent_indices = []
+    sorted_agent_recommendations = []
+
+    for agent in reversed(structured_array):
+        agent_index = agent_indices[agent[1]]
+        recommendation = agent[2]
+        if agent_index in sorted_agent_indices:
+            continue
+        else:
+            sorted_agent_indices.append(agent_index)
+            sorted_agent_recommendations.append(recommendation)
+
+    return sorted_agent_indices, sorted_agent_recommendations
+
+
+def assign_flexible_to_up_down(S, force_up, force_down, flexible, Q, arg_max_Q, method, estimate, minimize=False):
+    n_flexible = len(flexible)
+    n_up = len(force_up)
+    n_down = len(force_down)
+    diff_up_down = n_up - n_down
+    minority = 1 if diff_up_down >= 0 else 0
+
+    sorted_flexible, flexible_recommendation = sort_by_belief_improvement(
+        Q, arg_max_Q, flexible, minority, method, estimate, minimize=minimize)
+
+    if abs(diff_up_down) >= n_flexible:
+
+        while len(sorted_flexible) > 0:
+            S[sorted_flexible.pop()] = flexible_recommendation.pop()
+
+    elif abs(diff_up_down) < n_flexible:
+
+        for x in range(abs(diff_up_down) + int((n_flexible - abs(diff_up_down)) / 2)):
+            S[sorted_flexible.pop()] = flexible_recommendation.pop()
+
+        sorted_majority, majority_recommendation = sort_by_belief_difference(
+            Q, arg_max_Q, sorted_flexible, abs(1 - minority))
+
+        while len(sorted_majority) > 0:
+            S[sorted_majority.pop()] = majority_recommendation.pop()
+
+    return S
+
+
+def assign(S, Q, arg_max_Q, agent_indices, action, method, estimate, minimize=False):
+
+    sorted_agent_indices, sorted_agent_recommendations = sort_by_belief_improvement(
+        Q, arg_max_Q, agent_indices, action, method, estimate, minimize=minimize)
+
+    while len(sorted_agent_indices) > 0:
+        S[sorted_agent_indices.pop()] = sorted_agent_recommendations.pop()
+    return S
+
+
+def calculate_travel_time_estimate(n_cross, n_agents):
+    return [
+            1+(1-n_cross/n_agents)/2+n_cross/n_agents,  # up
+            1+(1-n_cross/n_agents)/2+n_cross/n_agents,  # down
+            1+n_cross/n_agents                          # cross
+            ]
+
+
+def optimized_heuristic_recommender(Q, n_agents, method="estimate", minimize=False):
+    S = np.zeros(n_agents)
+    arg_max_Q = np.argmax(Q, axis=2)
+
+    up, down, cross, flexible = split_up_down_cross_flexible(arg_max_Q)
+
+    estimate = calculate_travel_time_estimate(len(cross), n_agents)
+
+    assign_flexible_to_up_down(S, up, down, flexible, Q, arg_max_Q, method, estimate, minimize)
+
+    S = assign(S, Q, arg_max_Q, up, 0, method, estimate, minimize)
+    S = assign(S, Q, arg_max_Q, down, 1, method, estimate, minimize)
+    S = assign(S, Q, arg_max_Q, cross, 2, method, estimate, (not minimize))
+
+    return S.astype(int)
+
+
 def naive_recommender(Q, n_actions):
     initial_guess = np.random.randint(Q.shape[1], size=Q.shape[0])
     objective = total_welfare  # hard-coded to total welfare
