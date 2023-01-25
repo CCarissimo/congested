7f083c77c9053be602ea73386aa338dc03f2dca0
ccarissimo <N20kztu!>
2023-01-25 15:44:45 +0100

    partial update, to be completed

--- plot_experiments.py
+++ plot_experiments.py
@@ -34,8 +34,8 @@ def welfare(epsilon):
 
 
 def plot_case(axis, recommender, initialization, quantity, linestyle):
-    means.loc[recommender, initialization][quantity].plot(legend=True, label=recommender, ax=axis, linestyle=linestyle)
-    axis.fill_between(np.linspace(0, 0.2, 11),
+    l, = means.loc[recommender, initialization][quantity].plot(legend=True, label=recommender, ax=axis, linestyle=linestyle)
+    l, = axis.fill_between(np.linspace(0, 0.2, 11),
                      means.loc[recommender, initialization][quantity] + std.loc[recommender, initialization][quantity],
                      means.loc[recommender, initialization][quantity] - std.loc[recommender, initialization][quantity],
                      alpha=0.2)
@@ -53,6 +53,8 @@ plt.style.use('paper.mplstyle')
 # cmap = plt.get_cmap('plasma')
 sns.set_palette("magma")
 # colors = [cmap(c) for c in np.linspace(0.1, 0.9, n_actions)]
+
+recommenders = ["none", "random", "optimized_estimate_maximize", "aligned_heuristic"]
 
 fig, [ax, ax2] = plt.subplots(nrows=1, ncols=2, figsize=(6.75, 3.2))
 
@@ -92,7 +94,7 @@ ax2.set_xlabel(r"irrationality / exploration rate ($\epsilon$)", **{"fontname": 
 ax2.set_ylabel(r"recommendation to argmax alignment", **{"fontname": "Times New Roman", "fontsize": "x-large"})
 # ax2.legend(prop={"family": "Times New Roman", "size": "x-large"}, title="recommender type", bbox_to_anchor=(1.05, 1))
 
-plt.legend(fontsize="xx-large", bbox_to_anchor=(0.65, 1.15), ncol = 4)
+fig.legend(bbox_to_anchor=(0.5,0.95), loc='lower center', ncol = 4, handles = handles)
 # fig.tight_layout()
 plt.savefig("plots/recommenders_alignment_comparison.pdf")
 plt.show()
