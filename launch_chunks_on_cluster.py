75c90fb8bc81e1d38475ed722fcc6154898df08a
ccarissimo <N20kztu!>
2025-02-25 16:29:21 +0100

    more cores

--- launch_chunks_on_cluster.py
+++ launch_chunks_on_cluster.py
@@ -7,4 +7,4 @@ if __name__ == "__main__":
     for alpha in np.linspace(0.01, 1, grid_size):
         for epsilon in np.linspace(0, 0.5, grid_size):
             for gamma in np.linspace(0, 0.99, grid_size):
-                os.system(f"sbatch --time=12:00:00 --ntasks=32 --mem-per-cpu=4G --wrap='python3 ./chunking_parameter_simulations.py {alpha} {epsilon} {gamma}'")
+                os.system(f"sbatch --time=12:00:00 --ntasks=64 --mem-per-cpu=1G --wrap='python3 ./chunking_parameter_simulations.py {alpha} {epsilon} {gamma}'")
