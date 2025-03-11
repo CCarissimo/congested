f1fa25a907908292446c4a9ed1996de5c2ebb51b
ccarissimo <N20kztu!>
2025-03-11 15:00:01 +0100

    set time

--- monitor_running_chunks.py
+++ monitor_running_chunks.py
@@ -27,7 +27,7 @@ slurm_dir = "/cluster/home/ccarissimo/braessQlearning/"
 
 # print(jobs[0])
 
-start = 1740128574
+start = 1741701584
 now = time.time()
 elapsed = (now - start)/(60*60)
 total_jobs = 1000
