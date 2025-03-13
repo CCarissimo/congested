d23c5bb99fbc4f0de62ff62a12b2b7b8536899e3
ccarissimo <N20kztu!>
2025-03-13 11:21:30 +0100

    analysis pipeline

--- analysis/analysis_pipeline.py
+++ analysis/analysis_pipeline.py
@@ -1,23 +1,27 @@
 import join_dataframes
 import compute_best_responses
 import process_best_responses
+import pickle
 
 
-def main(path_to_data):
+def main(path_to_data, name="results_v0"):
 
     print("(1/3) joining dataframes")
-    df = join_dataframes.main(path_to_data+"dataframes/", path_to_data)
+    df = join_dataframes.all2df(path_to_data+"dataframes/")
+    df = join_dataframes.aggregate_dfs(df)
+    df.to_csv(path_to_data + f"{name}.csv")
 
     print("(2/3) computing best responses")
-    best_responses = compute_best_responses.main(df, path_to_data)
+    best_responses = compute_best_responses.compute_deviator_best_response(df)
+    with open(path_to_data + f"{name}_mp_br_indices.pkl", "wb") as file:
+        pickle.dump(best_responses, file)
 
     print("(3/3) processing multi_parameter best responses")
     final_df = process_best_responses.calculate_metric_changes_after_best_responses(df, best_responses, sub_df=None)
-
-    save_path_for_multi = f"{path_to_data}multi_parameter_profits_metric_after_best_responses.csv"
-    final_df.to_csv(save_path_for_multi)
+    final_df.to_csv(f"{path_to_data}{name}_mp_br_metrics.csv")
 
 
 if __name__ == "__main__":
     directory = "/cluster/work/coss/ccarissimo/braess_symmetric_meta_game_2/"
-    main(directory)
+    name = "braess_symmetric_results_v0"
+    main(directory, name)
