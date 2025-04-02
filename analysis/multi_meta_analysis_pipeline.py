958816aba4371d46a261fc5631a5494ac513bc8d
ccarissimo <N20kztu!>
2025-04-02 13:11:52 +0200

    separate by deviators to run in parallel on cluster

--- analysis/multi_meta_analysis_pipeline.py
+++ analysis/multi_meta_analysis_pipeline.py
@@ -5,12 +5,16 @@ import pickle
 import argparse
 
 
-def main(path_to_data, name="results_v0", deviators=None):
 
+def join(path_to_data, name):
     print("(1/3) joining dataframes")
-    df = join_dataframes.all2df(path_to_data+"dataframes/")
+    df = join_dataframes.all2df(path_to_data + "dataframes/")
     df = join_dataframes.aggregate_dfs(df)
     df.to_csv(path_to_data + f"{name}.csv")
+    return df
+
+
+def main(path_to_data, df, name="results_v0", deviators=None):
 
     if deviators is None:
         for n_deviators in [2, 3, 6, 12, 25, 50]:
@@ -25,6 +29,7 @@ def main(path_to_data, name="results_v0", deviators=None):
             print(f"N:{n_deviators} processing multi_parameter best responses")
             final_df = process_best_responses.calculate_metric_changes_after_best_responses(sub_df, best_responses, sub_df=None)
             final_df.to_csv(f"{path_to_data}{name}_deviators{n_deviators}_mp_br_metrics.csv")
+
     else:
         sub_df = df[df["number_of_deviators"] == deviators]
 
@@ -46,10 +51,20 @@ if __name__ == "__main__":
     # Add arguments
     parser.add_argument('directory', type=str, help="main directory which contains the dataframes directory")
     parser.add_argument('-n', '--name', type=str, help="name for file save", default="results_v0")
-    parser.add_argument('-d', '--deviators', type=str, help="what is the size of the deviator population", default=None)
+    parser.add_argument('-d', '--deviators', type=int, help="what is the size of the deviator population", default=None)
+    parser.add_argument('-j', '--joined', action='store_true', help="have the dataframes already been joined")
 
     # Parse the arguments
     args = parser.parse_args()
 
+    # join the dfs
+    if args.joined:
+        import pandas as pd
+        df = pd.read_csv(args.directory+args.name)
+    else:
+        df = join(args.directory, args.name)
+
     # Run the analysis
-    main(args.directory, args.name, args.deviators)
+    main(args.directory, df, args.name, args.deviators)
+
+# sbatch --time=12:00:00 --ntasks=1 --mem-per-cpu=16G --wrap='python3 ./multi_meta_analysis_pipeline.py /cluster/work/coss/ccarissimo/braess_symmetric_meta_game_core/ -n braess_symmetric_meta_game_results_v0 -d 2 -j '
